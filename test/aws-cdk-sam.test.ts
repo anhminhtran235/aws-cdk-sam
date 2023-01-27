6eaeaf3a83a523a7deb73b58379c8153b43047f5
Anh Minh Tran <anhminhtran235@gmail.com>
2023-01-26 23:04:18 -0800

    Initial commit

---
+++ test/aws-cdk-sam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkSam from '../lib/aws-cdk-sam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-sam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkSam.AwsCdkSamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
